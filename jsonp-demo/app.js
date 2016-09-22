ba9b0ab27a50aef56ea3c35d58820559854cafb7
bigdots <brand@yuzhigangdeMacBook-Pro.local>
2016-09-22 16:52:02 +0800

    modify jsonp demo

--- jsonp-demo/app.js
+++ jsonp-demo/app.js
@@ -9,21 +9,24 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// var swig = new swig.Swig();
-// app.engine('html', swig.renderFile);
-// app.set('view engine', 'html');
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
+// 设置路由
 app.get("/",function(req,res){
     res.render('index', { title: 'Express' });
 })
 
+// 模拟数据
 var data = {"brand":23}
 
+// 设置接口
 app.get("/index.json",function(req,res){
     var param = urlLib.parse(req.url,true);
-    var returnvalue = param.query.callback+ '(' + JSON.stringify(data) +')' ;
-    console.log(param.query)
-    res.send(returnvalue)
+    var returnValue = param.query.callback+ '(' + JSON.stringify(data)+')'
+    //将结果返回客户端
+    res.send(returnValue)
 })
 
 
